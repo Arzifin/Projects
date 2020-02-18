fad0ff0524415d8516ae747d6d1be3f13e05208d
Arzi <noobspin@hotmail.com>
2020-02-18 21:15:49 +0200

    Delete hello.java

--- hello.java
+++
@@ -1,9 +0,0 @@
-// Rehearsing and reinforcing my old git skills
-
-public class hello {
-	public static void main (String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("It's a beautiful day outside.");
-	}
-}
-
